dc024f01688fda216115c4ce72f698e65bb8a30e
Olivia Motevalli <olivia.motevalli.aliabadi@gmail.com>
2023-06-27 14:06:53 +0100

    undo for commit purposes

--- app.js
+++ app.js
@@ -8,16 +8,6 @@ app.get("/api/topics", getTopics);
 
 app.get("/api/", getAPI);
 
-app.get("/api/articles/:article_id", (req, res) => {
-    const { article_id } = req.params
-    console.log(article_id);
-})
-
-// deconstruct from the req body
-// pass onto controller/ model
-// use model to write a SQL query
-// mindful of SQL injection
-
 
 app.use("*", (req, res) => {
     res.status(404).send({msg: "Not found"})
