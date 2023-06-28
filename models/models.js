f1b5394383daf01b0b4460bdd5b1466f3a3243ab
Olivia Motevalli <olivia.motevalli.aliabadi@gmail.com>
2023-06-28 12:14:43 +0100

    update in line with feedback; refactored error handlers

--- models/models.js
+++ models/models.js
@@ -9,11 +9,9 @@ exports.selectTopics = () => {
 }
 
 exports.selectArticleByID = (article_id) => {
-    const sqlQuery = `SELECT * FROM articles WHERE article_id = $1;`;
     return db
-    .query(sqlQuery, [article_id])
+    .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
     .then((article) => {
-        console.log(article);
         if(article.rows.length === 0) {
             return Promise.reject({
                 status: 404,
