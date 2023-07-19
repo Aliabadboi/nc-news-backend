c84f8fdc018fadf68e9546d010eaafe9b35cf4bc
Olivia Motevalli <olivia.motevalli.aliabadi@gmail.com>
2023-07-19 13:25:49 +0100

    ticket 8, patch votes

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,45 +1,49 @@
-const { selectTopics, selectArticleByID, selectArticles, updateArticleVotes } = require("../models/articles.models");
-const data = require("../endpoints.json");
-const articles = require("../db/data/test-data/articles");
+const {
+  selectTopics,
+  selectArticleByID,
+  selectArticles,
+  updateArticleVotes,
+} = require('../models/articles.models');
+const data = require('../endpoints.json');
+const articles = require('../db/data/test-data/articles');
 
 exports.getTopics = (req, res, next) => {
-    selectTopics()
+  selectTopics()
     .then((topics) => {
-        res.status(200).send({ topics });
+      res.status(200).send({ topics });
     })
     .catch(next);
-}
+};
 
 exports.getAPI = (req, res) => {
-    res.status(200).send(data);
-}
+  res.status(200).send(data);
+};
 
 exports.getArticleByID = (req, res, next) => {
-    const { article_id } = req.params;
-    selectArticleByID(article_id)
+  const { article_id } = req.params;
+  selectArticleByID(article_id)
     .then((article) => {
-        res.status(200).send({ article });
+      res.status(200).send({ article });
     })
     .catch((err) => {
-        next(err)
+      next(err);
     });
-}
+};
 
 exports.getArticles = (req, res) => {
-    selectArticles()
-    .then((articles) => {
+  selectArticles().then((articles) => {
     res.status(200).send({ articles });
-})} 
+  });
+};
 
 exports.editArticleVotes = (req, res, next) => {
-    const { article_id } = req.params;
-    const votes = req.body;
-    console.log(votes.inc_votes, "<<<< VOTES, REQ BODY");
-    updateArticleVotes(article_id, votes.inc_votes)
+  const { article_id } = req.params;
+  const votes = req.body;
+  updateArticleVotes(article_id, votes.inc_votes)
     .then((article) => {
-        res.status(200).send({ article })
+      res.status(200).send({ article });
     })
     .catch((err) => {
-        next(err)
+      next(err);
     });
-}
+};
